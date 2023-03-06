48088464206fd4d161a3ef66767e2b1baf83a9a7
taeryu7 <pegasy02@gmail.com>
2023-03-06 17:24:58 +0900

    0306_after

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,201 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { darken, lighten } from 'polished';
+
+const colorStyles = css`
+  ${({ theme, color }) => {
+    const selected = theme.palette[color];
+    return css`
+      background: ${selected};
+      &:hover {
+        background: ${lighten(0.1, selected)};
+      }
+      &:active {
+        background: ${darken(0.1, selected)};
+      }
+      ${props =>
+        props.outline &&
+        css`
+          color: ${selected};
+          background: none;
+          border: 1px solid ${selected};
+          &:hover {
+            background: ${selected};
+            color: white;
+          }
+        `}
+    `;
+  }}
+`;
+
+const sizes = {
+  large: {
+    height: '3rem',
+    fontSize: '1.25rem'
+  },
+  medium: {
+    height: '2.25rem',
+    fontSize: '1rem'
+  },
+  small: {
+    height: '1.75rem',
+    fontSize: '0.875rem'
+  }
+};
+
+const sizeStyles = css`
+  ${({ size }) => css`
+    height: ${sizes[size].height};
+    font-size: ${sizes[size].fontSize};
+  `}
+`;
+
+const fullWidthStyle = css`
+  ${props =>
+    props.fullWidth &&
+    css`
+      width: 100%;
+      justify-content: center;
+      & + & {
+        margin-left: 0;
+        margin-top: 1rem;
+      }
+    `}
+`;
+
+const StyledButton = styled.button`
+  // 공통 스타일
+  display: inline-flex;
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  padding-left: 1rem;
+  padding-right: 1rem;
+
+  // 크기
+  ${sizeStyles}
+
+  // 색상
+  ${colorStyles}
+
+  // 기타
+  & + & {
+    margin-left: 1rem;
+  }
+
+  ${fullWidthStyle}
+`;
+
+function Button({ children, color, size, outline, fullWidth, ...rest }) {
+  return (
+    <StyledButton
+      color={color}
+      size={size}
+      outline={outline}
+      fullWidth={fullWidth}
+      {...rest}
+    >
+      {children}
+    </StyledButton>
+  );
+}
+
+Button.defaultProps = {
+  color: 'blue',
+  size: 'medium'
+};
+
+export default Button;
+
+/*
+//
+import React from 'react';
+import styled from 'styled-components';
+import { darken, lighten } from 'polished';
+
+const StyledButton = styled.button`
+  // 공통 스타일
+  display: inline-flex;
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  padding-left: 1rem;
+  padding-right: 1rem;
+
+  // 크기
+  height: 2.25rem;
+  font-size: 1rem;
+
+  // 색상
+  background: #228be6;
+  &:hover {
+    background: ${lighten(0.1, '#228be6')};
+  }
+  &:active {
+    background: ${darken(0.1, '#228be6')};
+  }
+
+  // 기타
+  & + & {
+    margin-left: 1rem;
+  }
+`;
+
+function Button({ children, ...rest }) {
+  return <StyledButton {...rest}>{children}</StyledButton>;
+}
+
+export default Button;
 
 
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  // 공통 스타일 
+  display: inline-flex;
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  padding-left: 1rem;
+  padding-right: 1rem;
+
+  // 크기
+  height: 2.25rem;
+  font-size: 1rem;
+
+  // 색상
+  background: #228be6;
+  &:hover {
+    background: #339af0;
+  }
+  &:active {
+    background: #1c7ed6;
+  }
+
+  // 기타
+  & + & {
+    margin-left: 1rem;
+  }
+`;
+
+function Button({ children, ...rest }) {
+  return <StyledButton {...rest}>{children}</StyledButton>;
+}
+
+export default Button;
+*/
+
+/*
 // rest props 전달하기
 
 // 필요한 이벤트가 있을 때 마다 매번 이렇게 넣어주는건 귀찮은 작업이다.
@@ -28,8 +224,7 @@ Button.defaultProps = {
 export default Button;
 // ...rest를 사용해서 우리가 지정한 props 를 제외한 값들을 rest 라는 객체에 모아주고, 
 // <button> 태그에 {...rest} 를 해주면, rest 안에 있는 객체안에 있는 값들을 모두 <button> 태그에 설정을 해준다.
-
-
+*/
 
 /*
 //전체 너비 차지하는 옵션
